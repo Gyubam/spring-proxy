56e1005f6ef216e210053cd78974d70a018d9d9e
sgb8170@naver.com <sgb8170@naver.com>
2022-10-26 22:13:45 +0900

    V2 - 구체 클래스 수동 빈 등록 설정

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-//@Import(AppConfigV1.class)
 @Import({AppConfigV1.class, AppConfigV2.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
